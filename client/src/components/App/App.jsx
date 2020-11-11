02b948de345d3324745e4e872b7702b8cf50c418
Sebastian Blanco <sebastianblancogonz@gmail.com>
2020-11-11 18:14:58 +0100

    :wrench: Fix styles

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, withRouter, Switch, Route } from 'react-router-dom'
 
 import { Home } from '..'
+import '../../styles/styles.css'
 
 const App = () => (
   <BrowserRouter>
